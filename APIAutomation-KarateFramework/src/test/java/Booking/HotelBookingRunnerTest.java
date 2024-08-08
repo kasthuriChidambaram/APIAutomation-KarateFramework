7de295077aef2aa7b253d1bb43f406230f1c4282
kasthuriChidambaram <nck8214@gmail.com>
2024-08-08 11:59:36 +0530

    Added Method for Parallel execution

--- APIAutomation-KarateFramework/src/test/java/Booking/HotelBookingRunnerTest.java
+++ APIAutomation-KarateFramework/src/test/java/Booking/HotelBookingRunnerTest.java
@@ -1,10 +1,26 @@
 package Booking;
 
 import com.intuit.karate.junit5.Karate;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 public class HotelBookingRunnerTest {
-    @Karate.Test
+
+
+ /*   @Karate.Test
+  //Non Parallel execution took 28 seconds to complete the regression run
     Karate testTags() {
         return Karate.run().tags("@regression").relativeTo(getClass());
+    }*/
+
+    @Test
+        //Parallel execution took 16 seconds to complete the regression run
+    void testParallel() {
+        Results results = Runner.path("classpath:Booking").tags("@regression").parallel(5);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
 }
